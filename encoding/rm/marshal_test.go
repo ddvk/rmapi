9cfad17758cab5d66b13be227e0b4333f310b84e
ddvk <ddvk>
2020-06-18 15:05:41 +0200

    draw some primitives

--- encoding/rm/marshal_test.go
+++ encoding/rm/marshal_test.go
@@ -3,10 +3,58 @@ package rm
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"testing"
 )
 
+func drawCircle(x, y, r float64) []Point {
+	theta := 0.0
+	dtheta := math.Pi / 100
+	points := make([]Point, 0)
+	for theta < 2*math.Pi {
+		cx := x + r*math.Cos(theta)
+		cy := y + r*math.Sin(theta)
+
+		p := Point{
+			X:         float32(cx),
+			Y:         float32(cy),
+			Speed:     2.,
+			Direction: 3.,
+			Width:     2.0,
+			Pressure:  .3,
+		}
+		points = append(points, p)
+		theta += dtheta
+	}
+	return points
+}
+
+func newPoint(x, y float32) Point {
+	p := Point{
+		X:         x,
+		Y:         y,
+		Speed:     2.,
+		Direction: 3.,
+		Width:     2.0,
+		Pressure:  .3,
+	}
+	return p
+}
+func drawRectange(x, y, w, h float32) []Point {
+	points := make([]Point, 0)
+	p := newPoint(x, y)
+	points = append(points, p)
+	p = newPoint(x+w, y)
+	points = append(points, p)
+	p = newPoint(x+w, y+h)
+	points = append(points, p)
+	p = newPoint(x, y+h)
+	points = append(points, p)
+	p = newPoint(x, y)
+	points = append(points, p)
+	return points
+}
 func testMarshalBinary(t *testing.T, fn string) {
 
 	points := make([]Point, 0)
@@ -32,7 +80,13 @@ func testMarshalBinary(t *testing.T, fn string) {
 						BrushSize:  Medium,
 						BrushColor: Black,
 						BrushType:  FinelinerV5,
-						Points:     points,
+						Points:     drawCircle(500, 500, 100),
+					},
+					Line{
+						BrushSize:  Medium,
+						BrushColor: Black,
+						BrushType:  FinelinerV5,
+						Points:     drawRectange(400, 50, 200, 200),
 					},
 					Line{
 						BrushSize:  Large,
@@ -41,7 +95,7 @@ func testMarshalBinary(t *testing.T, fn string) {
 						Points: []Point{
 							Point{
 								X:         100,
-								Y:         100,
+								Y:         400,
 								Speed:     2.,
 								Direction: 1.,
 								Width:     3.0,
